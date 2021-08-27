4f5f64e89e6c746a335cf5043f1f2d9ffd44661e
Mus <mustayeen@gmail.com>
2021-08-27 17:30:37 -0600

    First Commit: Test to see if it pushes

---
+++ Chapter 3/33selfcheck.py
@@ -0,0 +1,10 @@
+# write two Python functions to find the minimum number in a list. 
+# The first function should compare each number to every other number on the list O(n^2)
+# the second function should be linear O(n)
+
+def find_min_slow:
+
+    
+
+
+def find_min_fast:
